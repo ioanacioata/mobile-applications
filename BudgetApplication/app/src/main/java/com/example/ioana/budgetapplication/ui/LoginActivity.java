dbaf8f275456447ad5813f78d12e30143407fad8
Ioana Cioata <ioana.cioata26@gmail.com>
2018-01-11 18:37:51 +0200

    Added design for login and signup

--- BudgetApplication/app/src/main/java/com/example/ioana/budgetapplication/ui/LoginActivity.java
+++ BudgetApplication/app/src/main/java/com/example/ioana/budgetapplication/ui/LoginActivity.java
@@ -1,15 +1,31 @@
 package com.example.ioana.budgetapplication.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.ioana.budgetapplication.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViewById(R.id.buttonStartSignUp).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonStartSignUp:
+                finish();
+                startActivity(new Intent(this, SignUpActivity.class));
+                break;
+            case  R.id.buttonLogin:
+                break;
+        }
     }
 }
--- BudgetApplication/app/src/main/java/com/example/ioana/budgetapplication/ui/SignUpActivity.java
+++ BudgetApplication/app/src/main/java/com/example/ioana/budgetapplication/ui/SignUpActivity.java
@@ -1,15 +1,39 @@
 package com.example.ioana.budgetapplication.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.ioana.budgetapplication.R;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText editTextEmail;
+    EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        findViewById(R.id.textViewBackToLogin).setOnClickListener(this);
+        findViewById(R.id.buttonSignUp).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonSignUp:
+                registerUser();
+                break;
+            case  R.id.textViewBackToLogin:
+                finish();
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+        }
+    }
+
+    private void registerUser() {
+
     }
 }
